3f9362af8999c710453ec4f89c6676513de1ecfb
rizharafifr <rizharafif11@gmail.com>
2025-05-27 12:13:19 +0700

    feat: add login admin sidesa

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('pages.auth.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            $userStatus = Auth::user()->status;
+
+            if ($userStatus == 'submitted') {
+                return back()->withErrors(['email' => 'Akun anda masih menunggu persetujuan admin']);
+            } else if ($userStatus == 'rejected') {
+                return back()->withErrors(['email' => 'Akun anda ditolak oleh admin']);
+            }
+
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'Terjadi kesalahan. Perika kembali email dan password anda.',
+        ])->onlyInput('email');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
